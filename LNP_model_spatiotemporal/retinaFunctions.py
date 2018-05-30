5c1c7f9b6b82a330ec029146ee58c958b970db68
Jiakun Fu <jiakun.fu.bcm@gmail.com>
2018-05-30 13:07:42 -0500

    initial changes to syntax

--- LNP_model_spatiotemporal/retinaFunctions.py
+++ LNP_model_spatiotemporal/retinaFunctions.py
@@ -1,24 +1,22 @@
 from numpy import *
 from scipy import *
-from matplotlib.pyplot import *
-import pdb
-#from brian import *
 
-def RGCresponse(stimulus, RFradius, RFloc, RGCtype, sampleRate):
+
+def RGCresponse(stimulus, RFradius, RFloc, RGCtype, sample_rate):
     # INPUTS: stimulus x*y*time/sampleRate
     # INPUTS: RFradius in [0,1] space
     # INPUTS: RFloc, RF center in [0,1]x[0,1] space
     # INPUTS: RGCtype, 0 if ON, 1 if OFF
     
     # parameters
-    T = stimulus.shape[2]*sampleRate   # duration of stimulus in s
-    filterDuration = int(32/(1000/(1/sampleRate))) # must be integer
+    T = stimulus.shape[2] * sample_rate   # duration of stimulus in s
+    filter_duration = int(32 / (1000 / (1 / sample_rate))) # must be integer
     nlPoint = -0.9
     nlSlope = 2.
-    maxFiringRate = 100.
+    max_firing_rate = 100.
     #sampleRate = T/stimulus.shape[2]
-    onWeights  = .7  # center-surround
-    offWeights = -.2    # center-surround
+    on_weights  = .7  # center-surround
+    off_weights = -.2    # center-surround
     frequency  = 1.   # of biphasic filter
     phase      = -0.5    # of biphasic filter
     variance   = 1.    # variance of biphasic filter
@@ -27,67 +25,67 @@ def RGCresponse(stimulus, RFradius, RFloc, RGCtype, sampleRate):
 
     #pdb.set_trace()
     # assuming RFradius and RFloc are in [0,1] x [0,1] space
-    centerRadius = 0.5*RFradius*mean([stimulus.shape[0],stimulus.shape[1]])
+    center_radius = 0.5*RFradius*mean([stimulus.shape[0],stimulus.shape[1]])
     yLength = stimulus.shape[0]
     xLength = stimulus.shape[1]
-    pixelCenter = [int(RFloc[0]*xLength), int(RFloc[1]*yLength)]
-    pixelWidth  = [max(int(pixelCenter[0]-centerRadius),int(0)), min(int(pixelCenter[0]+centerRadius),int(stimulus.shape[0]-1))]
-    pixelHeight = [max(int(pixelCenter[1]-centerRadius),int(0)), min(int(pixelCenter[1]+centerRadius),int(stimulus.shape[1]-1))]
+    pixel_center = [int(RFloc[0]*xLength), int(RFloc[1]*yLength)]
+    pixel_width  = [max(int(pixel_center[0]-center_radius),int(0)), min(int(pixel_center[0]+center_radius),int(stimulus.shape[0]-1))]
+    pixel_height = [max(int(pixel_center[1]-center_radius),int(0)), min(int(pixel_center[1]+center_radius),int(stimulus.shape[1]-1))]
 
     if RGCtype == 0:    # ON type
-        spatialOutput = zeros(stimulus.shape[2])
-        for i in arange(pixelWidth[0], pixelWidth[1]+1):
-            for j in arange(pixelHeight[0], pixelHeight[1]+1):
-                if ((i-pixelCenter[0])**2+(j-pixelCenter[1])**2) < (centerRadius/2)**2:
-                    spatialOutput += stimulus[i,j,:]*onWeights
-                elif ((i-pixelCenter[0])**2+(j-pixelCenter[1])**2) < (centerRadius)**2:
-                    spatialOutput += stimulus[i,j,:]*offWeights
+        spatial_output = zeros(stimulus.shape[2])
+        for i in arange(pixel_width[0], pixel_width[1]+1):
+            for j in arange(pixel_height[0], pixel_height[1]+1):
+                if ((i-pixel_center[0])**2+(j-pixel_center[1])**2) < (center_radius/2)**2:
+                    spatial_output += stimulus[i,j,:]*on_weights
+                elif ((i-pixel_center[0])**2+(j-pixel_center[1])**2) < (center_radius)**2:
+                    spatial_output += stimulus[i,j,:]*off_weights
     elif RGCtype == 1:  # OFF type
-        spatialOutput = zeros(stimulus.shape[2])
-        for i in arange(pixelWidth[0], pixelWidth[1]+1):
-            for j in arange(pixelHeight[0], pixelHeight[1]+1):
-                if ((i-pixelCenter[0])**2+(j-pixelCenter[1])**2) < (centerRadius/2)**2:
-                    spatialOutput += stimulus[i,j,:]*offWeights
-                elif ((i-pixelCenter[0])**2+(j-pixelCenter[1])**2) < (centerRadius)**2:
-                    spatialOutput += stimulus[i,j,:]*onWeights
+        spatial_output = zeros(stimulus.shape[2])
+        for i in arange(pixel_width[0], pixel_width[1]+1):
+            for j in arange(pixel_height[0], pixel_height[1]+1):
+                if ((i-pixel_center[0])**2+(j-pixel_center[1])**2) < (center_radius/2)**2:
+                    spatial_output += stimulus[i,j,:]*off_weights
+                elif ((i-pixel_center[0])**2+(j-pixel_center[1])**2) < (center_radius)**2:
+                    spatial_output += stimulus[i,j,:]*on_weights
    #       spatialFilter = zeros((stimulus.shape[0],stimulus.shape[1]))
- #       for i in     arange(pixelWidth[0],  pixelWidth[1]-1):
- #           for j in arange(pixelHeight[0], pixelHeight[1]-1):
- #               if sqrt((i-pixelCenter[0])**2+(j-pixelCenter[1])**2) < centerRadius:
- #                   spatialFilter[i,j]     = onWeights
- #                   if sqrt((i-pixelCenter[0])**2+(j-pixelCenter[1])**2) < centerRadius/2:
- #                       spatialFilter[i,j] = offWeights
+ #       for i in     arange(pixel_width[0],  pixel_width[1]-1):
+ #           for j in arange(pixel_height[0], pixel_height[1]-1):
+ #               if sqrt((i-pixel_center[0])**2+(j-pixel_center[1])**2) < center_radius:
+ #                   spatialFilter[i,j]     = on_weights
+ #                   if sqrt((i-pixel_center[0])**2+(j-pixel_center[1])**2) < center_radius/2:
+ #                       spatialFilter[i,j] = off_weights
 
-#    spatialOutput = zeros(stimulus.shape[2])
+#    spatial_output = zeros(stimulus.shape[2])
 #    for t in range(stimulus.shape[2]):
 #        projection = array(stimulus[:,:,t])*array(spatialFilter)
-#        spatialOutput[t] = sum(projection)
+#        spatial_output[t] = sum(projection)
 
     #pdb.set_trace()
 
-    kernel = linearKernel(frequency,phase,variance,filterDuration)
-    linearOutput = convolve(spatialOutput,kernel,'valid')   # note this is max - min + 1
-    #nonlinearOutput = threshold(linearOutput, nlPoint, nlSlope)
+    kernel = linearKernel(frequency,phase,variance,filter_duration)
+    linearOutput = convolve(spatial_output,kernel,'valid')   # note this is max - min + 1
+    #nonlinear_output = threshold(linearOutput, nlPoint, nlSlope)
 
     # normalize nonlinear output to be between 0 and 1
-    #nonlinearOutput = nonlinearOutput - min(nonlinearOutput)
-    #nonlinearOutput = nonlinearOutput/max(nonlinearOutput)
-    nonlinearOutput = exp(linearOutput)/(1+exp(linearOutput))
+    #nonlinear_output = nonlinear_output - min(nonlinear_output)
+    #nonlinear_output = nonlinear_output/max(nonlinear_output)
+    nonlinear_output = exp(linearOutput)/(1+exp(linearOutput))
 
     spikes = []
-    spikeTimes = []
+    spike_times = []
     #timesGen = drange(0,T,sampleRate)
-    times = arange(0,len(nonlinearOutput))  # was stimulus.shape[2]
-    times = times*sampleRate
+    times = arange(0,len(nonlinear_output))  # was stimulus.shape[2]
+    times = times * sample_rate
     #times = ["%g" % x for x in timesGen]
-    print "{} time samples".format(len(times))
-    for t in range(len(nonlinearOutput)):
-        spikes.append(random.poisson(maxFiringRate*sampleRate*nonlinearOutput[t]))
+    print("{} time samples".format(len(times)))
+    for t in range(len(nonlinear_output)):
+        spikes.append(random.poisson(max_firing_rate * sample_rate * nonlinear_output[t]))
         if spikes[-1]:
             now = 1000.*array(times[t], float64) + random.rand(1)
-            spikeTimes.append(now)
+            spike_times.append(now)
 
-    return spikeTimes
+    return spike_times
 
 def linearKernel(freq,phase,variance,width):
     #needs to be fixed to be a proper high pass filter
